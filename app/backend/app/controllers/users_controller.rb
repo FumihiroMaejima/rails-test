a2be2e9f30c000cbdc6bf027a5901377e07cadae
Fumihiro_Maejima <testerwatson1101@gmail.com>
2023-12-18 01:22:36 +0900

    add email param.

--- app/backend/app/controllers/users_controller.rb
+++ app/backend/app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
     def show
       user = User.find(params[:id])
-      render json: user, only: [:id, :name, :gender, :address], status: :ok
+      render json: user, only: [:id, :name, :email, :updated_at], status: :ok
     rescue ActiveRecord::RecordNotFound
       render json: { error: 'User not found' }, status: :not_found
     end
